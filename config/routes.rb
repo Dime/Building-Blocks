85b6c63dce3417848c4c02990fa063bed26a61f5
Dime <dime@pashoski.com>
2010-07-16 15:03:46 +0200

    profile pages added, image holders changed, css tweaks to support the changes

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ ActionController::Routing::Routes.draw do |map|
   map.static_page ':page',
     :controller => 'static_page',
     :action => 'show',
-    :page => Regexp.new(%w[about contact sponsorship step2].join('|'))
+    :page => Regexp.new(%w[about contact sponsorship child_profile child_profile_gallery child_profile_videos].join('|'))
 
   map.root :controller => 'home', :action => 'index' # a replacement for public/index.html, with unique layout
 
